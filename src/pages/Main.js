5d19f78038a90242586c58d089c27bb52bb44229
seyeonn <ansse17@gmail.com>
2024-08-20 00:29:25 +0900

    숫자 입력 받아 달력 형식의 테이블 생성

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Topic from '../components/Topic';
+import HabitList from '../components/HabitList';
+
+function Main() {
+  return (
+    <div>
+        <Topic/>
+        <HabitList/>
+    </div>
+  );
+}
+
+export default Main;
